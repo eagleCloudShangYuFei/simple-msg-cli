a2634f6b441fe4fb44ebb1ccbdf0773669477f84
shangyf <shangyufei@eaglecloud.com>
2023-07-11 21:37:43 +0800

    fix

--- bin/i18n-pick.js
+++ bin/i18n-pick.js
@@ -3,9 +3,9 @@
 
 const program = require('commander');
 console.log('111');
-// program
-//   .version('1.5.0')
-//   .command('scan [path]', '扫描 React 项目')
-//   .command('pick', '替换文案')
-//   .command('export', '导出文案')
-//   .parse(process.argv);
+program
+  .version('1.5.0')
+  .command('scan [path]', '扫描 React 项目')
+  .command('pick', '替换文案')
+  .command('export', '导出文案')
+  .parse(process.argv);
