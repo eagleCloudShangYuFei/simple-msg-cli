5cb41bc8e5f46f57695393d75e4eda4e668eaee0
shangyf <shangyufei@eaglecloud.com>
2023-07-11 21:42:44 +0800

    fix

--- bin/i18n-pick.js
+++ bin/i18n-pick.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
  // vim: set ft=javascript:
 
+ (function() {
+  var childProcess = require("child_process");
+  var oldSpawn = childProcess.spawn;
+  function mySpawn() {
+      console.log('spawn called');
+      console.log(arguments);
+      var result = oldSpawn.apply(this, arguments);
+      return result;
+  }
+  childProcess.spawn = mySpawn;
+})();
+
 const program = require('commander');
 console.log('111');
 program
